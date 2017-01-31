48df86833792f12cbc8a1e32fcd26fbf101d515e
Rob Adams <robert.andrew.adams@gmail.com>
2017-01-31 11:32:07 +0000

    working websocket example

--- wstest/chat/consumers.py
+++ wstest/chat/consumers.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from channels.handler import AsgiHandler
+from channels import Group
 
 def http_consumer(message):
     # Make standard HTTP response - access ASGI path attribute directly
@@ -7,3 +8,17 @@ def http_consumer(message):
     # Encode that response into message format (ASGI)
     for chunk in AsgiHandler.encode_response(response):
         message.reply_channel.send(chunk)
+
+def ws_message(message):
+    Group("chat").send({
+        "text": "[user] %s" % message.content['text'],
+    })
+
+# Connected to websocket.connect
+def ws_add(message):
+    message.reply_channel.send({"accept": True})
+    Group("chat").add(message.reply_channel)
+
+# Connected to websocket.disconnect
+def ws_disconnect(message):
+    Group("chat").discard(message.reply_channel)
