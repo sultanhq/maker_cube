f15502abbe79a64fc418c6e0dc44852b79d50dac
Rob Adams <robert.andrew.adams@gmail.com>
2017-02-06 14:34:49 +0000

    working slack request function

--- django-rpi/maker_cube_app/static/cube/js/slack.js
+++ django-rpi/maker_cube_app/static/cube/js/slack.js
@@ -1,2 +1,17 @@
-console.log(window.key());
-console.log(window.token());
+var getSlackMessage = function() {
+  var a;
+  $.when(ajaxSlack()).done(function(data) {
+    a = data.messages[0].text;
+    document.getElementById("1-4").innerHTML = "<p>" + a + "</p>";
+  });
+};
+
+function ajaxSlack() {
+  var yourJsonData = "";
+  return $.ajax({
+    url: 'https://slack.com/api/channels.history?token=' + window.key() +
+      '&channel=' + window.token() + '&count=1&pretty=1',
+    dataType: "json",
+    data: yourJsonData,
+  });
+}
